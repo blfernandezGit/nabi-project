f73673402696decbdc8ca410e194fd467d493397
blfernandezGit <blfernandez@up.edu.ph>
2022-01-20 19:10:53 +0800

    add admin authorization routes

---
+++ src/components/Routes/AdminAuthRoute.js
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from 'react-router-dom'
+
+const AdminAuthRoute = ({ currentUser }) => {
+    const userDetails = currentUser?.details
+    const isAdmin = userDetails?.is_admin
+    return isAdmin ? <Outlet/> : <Navigate replace to={`/app/${userDetails?.username}`} />
+}
+
+export default AdminAuthRoute;
--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,15 +1,24 @@
 import Home from '../MainApp/Home'
+import AdminHome from '../MainApp/Admin'
+import Users from '../MainApp/Users'
 import Login from '../MainApp/Login'
 import Register from "../MainApp/Register"
 import AuthRoute from './AuthRoute';
 import ReverseAuthRoute from './ReverseAuthRoute'
+import AdminAuthRoute from './AdminAuthRoute'
+import Project from '../MainApp/Projects'
 
 const Index = ({ Routes, Route, isSignedIn, currentUser }) => {
   return (
     <>
       <Routes>
         <Route exact path = "/" element = { <AuthRoute isSignedIn = { isSignedIn }/> }>
-          <Route exact path = "/app/:username" element = { <Home/> }/>
+          <Route exact path = "app/:username" element = { <Home/> }/>
+          <Route exact path = "app/:username/admin" element = { <AdminAuthRoute currentUser = { currentUser }/> }>
+            <Route exact path = "" element = { <AdminHome/> }/>
+            <Route exact path = "users" element = { <Users/> }/>
+            <Route exact path = "projects" element = { <Project/> }/>
+          </Route>
         </Route>
         <Route exact path = "/register" element = { <ReverseAuthRoute isSignedIn = { isSignedIn } currentUser = { currentUser }/> }>
           <Route exact path = "/register" element = { <Register/> }/>
