4db68b7e44c851ac480885325fd2c79fca04b926
blfernandezGit <blfernandez@up.edu.ph>
2022-01-23 22:51:40 +0800

    Add temporary sidebar

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -11,22 +11,23 @@ import AdminUserProjects from '../MainApp/Admin/Users/Projects'
 import Projects from '../MainApp/Projects'
 import Project from '../MainApp/Projects/ProjectPage'
 import Ticket from '../MainApp/Projects/ProjectPage/TicketPage'
+import Sidebar from '../MainApp/Layout/Sidebar'
 
 const Index = ({ Routes, Route, isSignedIn, currentUser }) => {
   return (
     <>
       <Routes>
         <Route exact path = "/" element = { <AuthRoute isSignedIn = { isSignedIn }/> }>
-          <Route exact path = "app/:username" element = { <Home/> }/>
-          <Route exact path = "app/:username/projects" element = { <Projects/> }/>
-          <Route exact path = "app/:username/projects/:code" element = { <Project/> }/>
-          <Route exact path = "app/:username/projects/:code/:ticket_no" element = { <Ticket/> }/>
+          <Route exact path = "app/:username" element = { <Sidebar><Home/></Sidebar> }/>
+          <Route exact path = "app/:username/projects" element = { <Sidebar><Projects/></Sidebar> }/>
+          <Route exact path = "app/:username/projects/:code" element = { <Sidebar><Project/></Sidebar> }/>
+          <Route exact path = "app/:username/projects/:code/:ticket_no" element = { <Sidebar><Ticket/></Sidebar> }/>
           <Route exact path = "app/:username/admin" element = { <AdminAuthRoute currentUser = { currentUser }/> }>
-            <Route exact path = "" element = { <Home/> }/>
-            <Route exact path = "users" element = { <AdminUsers/> }/>
-            <Route exact path = "users/:user_username" element = { <AdminUserProjects/> }/>
-            <Route exact path = "projects" element = { <AdminProjects/> }/>
-            <Route exact path = "projects/:project_code" element = { <AdminProjectUsers/> }/>
+            <Route exact path = "" element = { <Sidebar><Home/></Sidebar> }/>
+            <Route exact path = "users" element = { <Sidebar><AdminUsers/></Sidebar> }/>
+            <Route exact path = "users/:user_username" element = { <Sidebar><AdminUserProjects/></Sidebar> }/>
+            <Route exact path = "projects" element = { <Sidebar><AdminProjects/></Sidebar> }/>
+            <Route exact path = "projects/:project_code" element = { <Sidebar><AdminProjectUsers/></Sidebar>}/>
           </Route>
         </Route>
         <Route exact path = "/register" element = { <ReverseAuthRoute isSignedIn = { isSignedIn } currentUser = { currentUser }/> }>
